9209948896d08e29978d30e4ede2c39e5f67ec3d
07AJAYBHATKAR <108801533+07AJAYBHATKAR@users.noreply.github.com>
2022-07-06 23:46:39 +0530

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+// const firebaseConfig = {
+//     apiKey: "AIzaSyDZZyZcXmBz-8cK8kYgwJGiBqz8atYElBE",
+//     authDomain: "group--11.firebaseapp.com",
+//     databaseURL: "https://group--11-default-rtdb.firebaseio.com",
+//     projectId: "group--11",
+//     storageBucket: "group--11.appspot.com",
+//     messagingSenderId: "240246651726",
+//     appId: "1:240246651726:web:8004d1e6d01089a125bc1f",
+//     measurementId: "G-HNDM6V37XZ"
+//   };
+  
